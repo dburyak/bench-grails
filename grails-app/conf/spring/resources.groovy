4008afba0cd55edafb6c228d9e832ee2e26e99b3
Dmytro Buryak <dmytro.buryak@gmail.com>
2019-09-01 19:22:28 +0300

    Iintial grails 4.0.0 project skeleton

---
+++ grails-app/conf/spring/resources.groovy
@@ -0,0 +1,6 @@
+import dburyak.benchmark.grails4.PrimeNumbersGenerator
+
+// Place your Spring DSL code here
+beans = {
+    primeNumbersGenerator(PrimeNumbersGenerator)
+}
---
+++ grails-app/init/dburyak/benchmark/grails4/BootStrap.groovy
@@ -0,0 +1,9 @@
+package dburyak.benchmark.grails4
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
